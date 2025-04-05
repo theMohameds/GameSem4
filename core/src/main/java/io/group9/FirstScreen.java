6237fa892bcc7a067aead124f1d02f8ad8c3c657
Florentin Mustafa <flmus23@student.sdu.dk>
2025-04-05 16:18:07 +0200

    Added a FirstScreen and removed the code from the main class

---
+++ core/src/main/java/io/group9/FirstScreen.java
@@ -0,0 +1,106 @@
+package io.group9;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import io.group9.plugins.ECSPlugin;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
+
+public class FirstScreen implements Screen {
+    private Box2DDebugRenderer debugRenderer;
+    private World world;
+    private Engine engine;
+    private final File pluginsDir = new File(System.getProperty("user.dir"), "mods");
+
+
+    @Override
+    public void show() {
+        world = new World(new Vector2(0, -10), true);
+        engine = new Engine();
+        loadPluginsFromMods(); // Load all JARs into a single class loader
+
+        ServiceLoader<ECSPlugin> serviceLoader = loadPluginsFromClasspath();
+
+        for (ECSPlugin plugin : serviceLoader) {
+            plugin.registerSystems(engine);
+            plugin.createEntities(engine);
+            System.out.println("Loaded plugin: " + plugin.getClass().getName() + "\n");
+        }
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        world.step(1/60f * delta, 6, 2);
+        engine.update(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Adjust your camera's viewport if needed
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        // Clean up resources
+        if (world != null) {
+            world.dispose();
+        }
+        if (debugRenderer != null) {
+            debugRenderer.dispose();
+        }
+    }
+
+    private void loadPluginsFromMods() {
+        addJarsToClasspath(pluginsDir);
+    }
+    private void addJarsToClasspath(File directory) {
+        List<URL> jarUrls = new ArrayList<>();
+        if (directory.exists() && directory.isDirectory()) {
+            File[] jars = directory.listFiles((dir, name) -> name.endsWith(".jar"));
+            if (jars != null) {
+                for (File jar : jars) {
+                    try {
+                        URL url = jar.toURI().toURL();
+                        jarUrls.add(url);
+                        System.out.println("Added JAR: " + jar.getName() + "\n");
+                    } catch (Exception e) {
+                        System.err.println("Error loading JAR: " + jar.getName());
+                        e.printStackTrace();
+                    }
+                }
+            }
+            if (!jarUrls.isEmpty()) {
+                URLClassLoader classLoader = new URLClassLoader(
+                    jarUrls.toArray(new URL[0]),
+                    getClass().getClassLoader()
+                );
+                Thread.currentThread().setContextClassLoader(classLoader);
+            }
+        } else {
+            System.err.println("Directory not found: " + directory.getPath());
+        }
+    }
+    private ServiceLoader<ECSPlugin> loadPluginsFromClasspath() {
+        return ServiceLoader.load(ECSPlugin.class, Thread.currentThread().getContextClassLoader());
+    }
+}
--- core/src/main/java/io/group9/Main.java
+++ core/src/main/java/io/group9/Main.java
@@ -1,74 +1,13 @@
 package io.group9;
 
-import com.badlogic.ashley.core.Engine;
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.physics.box2d.World;
-import io.group9.plugins.ECSPlugin;
+import com.badlogic.gdx.Game;
 
-import java.io.File;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ServiceLoader;
-
-public class Main extends ApplicationAdapter {
-    private World world;
-    private Engine engine;
-    private final File pluginsDir = new File(System.getProperty("user.dir"), "mods");
-
+public class Main extends Game {
+    FirstScreen firstScreen;
     @Override
     public void create() {
-        engine = new Engine();
-        loadPluginsFromMods(); // Load all JARs into a single class loader
-
-        ServiceLoader<ECSPlugin> serviceLoader = loadPluginsFromClasspath();
-
-        for (ECSPlugin plugin : serviceLoader) {
-            plugin.registerSystems(engine);
-            plugin.createEntities(engine);
-            System.out.println("Loaded plugin: " + plugin.getClass().getName() + "\n");
-        }
+        firstScreen = new FirstScreen();
+        setScreen(firstScreen);
     }
 
-    // Other methods (render, dispose) remain unchanged
-
-    private void loadPluginsFromMods() {
-        addJarsToClasspath(pluginsDir);
-    }
-
-    private void addJarsToClasspath(File directory) {
-        List<URL> jarUrls = new ArrayList<>();
-        if (directory.exists() && directory.isDirectory()) {
-            File[] jars = directory.listFiles((dir, name) -> name.endsWith(".jar"));
-            if (jars != null) {
-                for (File jar : jars) {
-                    try {
-                        URL url = jar.toURI().toURL();
-                        jarUrls.add(url);
-                        System.out.println("Added JAR: " + jar.getName() + "\n");
-                    } catch (Exception e) {
-                        System.err.println("Error loading JAR: " + jar.getName());
-                        e.printStackTrace();
-                    }
-                }
-            }
-            if (!jarUrls.isEmpty()) {
-                URLClassLoader classLoader = new URLClassLoader(
-                    jarUrls.toArray(new URL[0]),
-                    getClass().getClassLoader()
-                );
-                Thread.currentThread().setContextClassLoader(classLoader);
-            }
-        } else {
-            System.err.println("Directory not found: " + directory.getPath());
-        }
-    }
-
-    private ServiceLoader<ECSPlugin> loadPluginsFromClasspath() {
-        return ServiceLoader.load(ECSPlugin.class, Thread.currentThread().getContextClassLoader());
-    }
 }
