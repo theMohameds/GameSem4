8c0ffea4cdfb4f18fee5fac3c4e340ebccdeb6c4
CSteentoft <chste23@student.sdu.dk>
2025-04-23 13:25:30 +0200

    Death for player

--- Common/src/main/java/components/CollisionCategories.java
+++ Common/src/main/java/components/CollisionCategories.java
@@ -4,11 +4,10 @@ public class CollisionCategories {
     public static final short PLAYER = 0x0001;  // 0001
     public static final short ENEMY  = 0x0002;  // 0010
     public static final short ATTACK = 0x0004;  // 0100
-
     public static final short ENEMY_HURTBOX = 0x0008;
     public static final short ENEMY_FEET = 0x0006;
     public static final short GROUND = 0x0005;
     public static final short WALL = 0x0007;
+    public static final short ENEMY_ATTACK = 0x0400;
 
-    // Optionally, add more, such as ground, projectiles, etc.
 }
--- Player/src/main/java/io/group9/player/AttackContactReceiver.java
+++ Player/src/main/java/io/group9/player/AttackContactReceiver.java
@@ -3,28 +3,49 @@ package io.group9.player;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Contact;
 import io.group9.ContactReceiver;
+import io.group9.CoreResources;
+import io.group9.player.components.PlayerComponent;
 
 public class AttackContactReceiver implements ContactReceiver {
     @Override
     public void beginContact(Contact contact) {
-        Object fixDataA = contact.getFixtureA().getUserData();
-        Object fixDataB = contact.getFixtureB().getUserData();
+        // Fixture userData
+        Object fA = contact.getFixtureA().getUserData();
+        Object fB = contact.getFixtureB().getUserData();
+        // Body userData
+        Object bA = contact.getFixtureA().getBody().getUserData();
+        Object bB = contact.getFixtureB().getBody().getUserData();
 
-        boolean aIsPlayerAttack   = "playerAttack".equals(fixDataA);
-        boolean bIsPlayerAttack   = "playerAttack".equals(fixDataB);
+        // Did an enemyAttack sensor hit the player body?
+        boolean aIsEnemyAtk   = "enemyAttack".equals(fA);
+        boolean bIsEnemyAtk   = "enemyAttack".equals(fB);
+        boolean aIsPlayerBody = "player".equals(bA);
+        boolean bIsPlayerBody = "player".equals(bB);
 
-        boolean aIsEnemyHurtbox  = "enemyHurtbox".equals(fixDataA);
-        boolean bIsEnemyHurtbox  = "enemyHurtbox".equals(fixDataB);
-
-        // Check collisions:
-        if (aIsPlayerAttack && bIsEnemyHurtbox) {
-            Gdx.app.log("AttackContactReceiver", "Enemy was hit by player's attack (case A)!");
-        } else if (bIsPlayerAttack && aIsEnemyHurtbox) {
-            Gdx.app.log("AttackContactReceiver", "Enemy was hit by player's attack (case B)!");
+        if ((aIsEnemyAtk && bIsPlayerBody) || (bIsEnemyAtk && aIsPlayerBody)) {
+            hitPlayer();
         }
     }
 
     @Override
     public void endContact(Contact contact) { }
-}
 
+    private void hitPlayer() {
+        if (CoreResources.getPlayerEntity() == null) return;
+        PlayerComponent pc = CoreResources
+            .getPlayerEntity()
+            .getComponent(PlayerComponent.class);
+        if (pc == null || pc.state == PlayerComponent.State.DEAD) return;
+
+        pc.health -= 10;
+        if (pc.health <= 0) {
+            pc.state = PlayerComponent.State.DEAD;
+            Gdx.app.log("AttackContactReceiver", "Player has died!");
+        } else {
+            pc.state     = PlayerComponent.State.HURT;
+            pc.hurtTimer = pc.hurtDuration;
+            pc.isHurt    = true;
+            Gdx.app.log("AttackContactReceiver", "Player was hurt! HP=" + pc.health);
+        }
+    }
+}
